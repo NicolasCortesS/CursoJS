ad2e6e5f5142e8e70b1609b5a8b9bf8c837e4266
Nicolas <ncortes.silva88@gmail.com>
2024-01-16 10:27:28 -0300

    Inicio/termino Modulo Strings

---
+++ Strings.js
@@ -0,0 +1,7 @@
+let nombreCompleto = "Nicolás Ignacio Cortés Silva";
+let Edad = "24";
+let paisNacimiento = "Chile";
+
+console.log(nombreCompleto);
+console.log(Edad);
+console.log(paisNacimiento);
