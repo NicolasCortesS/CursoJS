95972fbfdff30af579e163d2208c5024c38245d8
Nicolas <ncortes.silva88@gmail.com>
2024-01-16 09:41:55 -0300

    Primer commit

---
+++ Ejercicio1.js
@@ -0,0 +1,24 @@
+var nombre = "Nicolas";
+
+console.log(nombre);
+
+let apellido = "Cortes";
+
+{
+    nombre = "Ignacio";
+    console.log(nombre);
+    console.log(apellido);
+    apellido = "Silva"
+    console.log(apellido);
+    
+}
+
+console.log(apellido);
+
+const edad = 24;
+console.log(edad);
+
+
+
+
+
