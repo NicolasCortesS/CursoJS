0dafeea76961640cd0769351bf517a878a609dd9
Nicolas <ncortes.silva88@gmail.com>
2024-01-17 07:45:25 -0300

    Añadimos ejercicios de Ciclos y archivo de Objetos

---
+++ Ciclos.js
@@ -0,0 +1,31 @@
+let min = 1;
+let max = 5;
+
+let sumaAcumulada = 0;
+
+for (min;min<=max;min++){
+    sumaAcumulada =  sumaAcumulada + min;
+    console.log(sumaAcumulada);
+}
+
+min = 1;
+max = 5;
+
+sumaAcumulada = 0;
+
+while(min<=max){
+    sumaAcumulada = sumaAcumulada + min;
+    min++;
+    console.log(sumaAcumulada);
+}
+
+min = 1;
+max = 5;
+
+sumaAcumulada = 0;
+
+do{
+    sumaAcumulada = sumaAcumulada + min;
+    min++;
+    console.log(sumaAcumulada);
+}while(min<=max)
