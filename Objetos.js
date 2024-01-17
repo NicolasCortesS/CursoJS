39b0d939f5218b1ca06880800da1be9f0f6f3ac5
NicolasCortesS <155210131+NicolasCortesS@users.noreply.github.com>
2024-01-17 09:10:20 -0300

    Update Objetos.js

--- Objetos.js
+++ Objetos.js
@@ -20,15 +20,6 @@ console.log(persona.lang)
 persona.lang = "en";
 console.log(persona.lang)
 
-
-/*for (x in persona){
-    console.log(x)
-    console.log(persona[x]);
-}
-    console.log(persona)
-
- console.log(persona.nombrecompleto())*/
-
 function Personaje(Nombre,Faccion,Raza,Clase){
     this.nombre = Nombre;
     this.faccion = Faccion;
@@ -39,10 +30,10 @@ function Personaje(Nombre,Faccion,Raza,Clase){
 let menpho = new Personaje("Menpho","Horda","Trol","Mago")
 let titin = new Personaje("Titin","Horda","Goblin","Monje")
 
-let personajeMenpho = Object.values(menpho);
-
 console.log(menpho);
 console.log(titin);
+
+let personajeMenpho = Object.values(menpho);
 
 for( let x = 0; x<personajeMenpho.length; x++){
     console.log(personajeMenpho[x])
