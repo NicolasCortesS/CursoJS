6112a95a805ac2a0738d091337127b17bc7d2916
Nicolas <ncortes.silva88@gmail.com>
2024-01-17 08:34:35 -0300

    Avance Objetos

--- Objetos.js
+++ Objetos.js
@@ -1,1 +1,34 @@
- 
+let persona = {
+    nombre: "Nicolás",
+    apellido: "Cortés",
+    email: "ncortes.silva88@gmail.com",
+    edad: 24,
+    idioma: "es",
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase();
+    },
+    get nombreCompleto(){
+        return this.nombre + " " + this.apellido;
+    }
+ }
+
+
+console.log(persona.lang)
+persona.lang = "en";
+console.log(persona.lang)
+
+
+/*for (x in persona){
+    console.log(x)
+    console.log(persona[x]);
+}
+    console.log(persona)
+
+ console.log(persona.nombrecompleto())*/
+
+
+
+
